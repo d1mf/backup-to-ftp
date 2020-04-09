d76dfaad6fabad81ba1bc52b79e75a8cca703980
Dmytro Babyk <dmytro@system-admin.work>
2020-04-09 23:59:53 +0300

    Update backup_to_ftp.sh

--- backup_to_ftp.sh
+++ backup_to_ftp.sh
@@ -27,7 +27,7 @@ PORT=21
 BACKUPDIR="/"
 
 # Backups older than ndays will be removed
-ndays=5
+ndays=1
 
 # The absolute local directory to pickup *.tar.gz file
 # Please do not end the path with a forward slash.
